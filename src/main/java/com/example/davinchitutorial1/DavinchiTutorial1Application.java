5c8a7ef44b4497275b2edaaae0daa8b222c3edd7
Suawolf <lcgswolf@gmail.com>
2026-05-06 16:04:30 -0600

    Project intialized

---
+++ src/main/java/com/example/davinchitutorial1/DavinchiTutorial1Application.java
@@ -0,0 +1,13 @@
+package com.example.davinchitutorial1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavinchiTutorial1Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(DavinchiTutorial1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/davinchitutorial1/DavinchiTutorial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.davinchitutorial1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavinchiTutorial1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
