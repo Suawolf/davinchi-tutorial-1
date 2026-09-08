b12223f3581f14edea87771c3b0b898b0049abe5
Suawolf <lcgswolf@gmail.com>
2026-09-08 17:35:05 -0600

    JPA Video 7 Completed! Queries
    Signed-off-by: Suawolf <lcgswolf@gmail.com>

--- src/main/java/edu/tutorial/davinchi1/product/infrastructure/database/repository/QueryProductRepository.java
+++ src/main/java/edu/tutorial/davinchi1/product/infrastructure/database/repository/QueryProductRepository.java
@@ -3,10 +3,30 @@ package edu.tutorial.davinchi1.product.infrastructure.database.repository;
 import edu.tutorial.davinchi1.product.domain.entity.Product;
 import edu.tutorial.davinchi1.product.infrastructure.database.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QueryProductRepository extends JpaRepository<ProductEntity,Long> {
+    List<ProductEntity> findByNameContaining(String name);
+
+    List<ProductEntity> findAllByPriceBetween(Double priceStart, Double priceEnd);
+
+    @Query("""
+            SELECT p
+            FROM ProductEntity p
+            WHERE p.name LIKE concat('%', :name, '%')
+            OR p.description LIKE concat('%', :description, '%')
+            OR p.price BETWEEN :priceAfter AND :priceBefore
+            """)
+    List<ProductEntity> findProductDetails(String name, String description, Double priceAfter, Double priceBefore);
+
+    boolean existsByName(String name);
+
+    long countByPriceBetween(Double priceStart, Double priceEnd);
+
 
 
 }
